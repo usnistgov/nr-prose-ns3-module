5c619eea01476803d26520c3feebd0587da7d929
Tom Henderson <tomh@tomh.org>
2024-08-13 16:28:59 -0700

    Change all 3GPP channel conditions to line-of-sight

--- examples/nr-prose-network-coex.cc
+++ examples/nr-prose-network-coex.cc
@@ -363,6 +363,7 @@ main(int argc, char* argv[])
     bwp0->m_channelBandwidth = bandwidthCc0Bpw0;
     bwp0->m_lowerFrequency = bwp0->m_centralFrequency - bwp0->m_channelBandwidth / 2;
     bwp0->m_higherFrequency = bwp0->m_centralFrequency + bwp0->m_channelBandwidth / 2;
+    bwp0->m_scenario = BandwidthPartInfo::Scenario::RMa_LoS;
 
     cc0->AddBwp(std::move(bwp0));
 
@@ -372,6 +373,7 @@ main(int argc, char* argv[])
     bwp1->m_channelBandwidth = bandwidthCc0Bpw1;
     bwp1->m_lowerFrequency = bwp1->m_centralFrequency - bwp1->m_channelBandwidth / 2;
     bwp1->m_higherFrequency = bwp1->m_centralFrequency + bwp1->m_channelBandwidth / 2;
+    bwp1->m_scenario = BandwidthPartInfo::Scenario::RMa_LoS;
 
     cc0->AddBwp(std::move(bwp1));
 
